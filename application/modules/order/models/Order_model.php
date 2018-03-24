e6700017b1133962726bb9993e59b14339fde268
ananthuva <ava007jb@gmail.com>
2018-03-24 14:23:43 +0530

    return status id on all apis

--- application/modules/order/models/Order_model.php
+++ application/modules/order/models/Order_model.php
@@ -69,7 +69,7 @@ class Order_model extends SYS_Model {
         $limit = empty($limit) ? 100 : $limit;
         $offset = empty($limit) ? 0 : $offset;
         
-        $this->db->select('o.id,o.order_bill_id,s.name as store_name,o.note,os.order_status_name,CONCAT_WS(" ",u.name,u.lname) as user_name,'
+        $this->db->select('o.id,o.order_bill_id,s.name as store_name,o.note,o.status as order_status_id,os.order_status_name,CONCAT_WS(" ",u.name,u.lname) as user_name,'
                 . ' o.order_date,o.payment_type,o.payment_status,o.created_on');
         $this->db->from('order o');
         $this->db->join('table_order_status os', 'os.order_status_id = o.status', 'left');
@@ -85,7 +85,7 @@ class Order_model extends SYS_Model {
      * This function is used to get order details 
      */
     public function getOrderdetails($id) {
-        $this->db->select('o.id,o.order_bill_id,s.name as store_name,o.note,os.order_status_name,CONCAT_WS(" ",u.name,u.lname) as user_name,'
+        $this->db->select('o.id,o.order_bill_id,s.name as store_name,o.note,o.status as order_status_id,os.order_status_name,CONCAT_WS(" ",u.name,u.lname) as user_name,'
                 . ' o.order_date,o.payment_type,o.payment_status,o.created_on,o.user_id');
         $this->db->where('o.id', $id);
         $this->db->from('order o');
